11c7b55e3b3bcbb5c3703e69d509105db29424ed
MoaLindgren <moa.lindgren@outlook.com>
2018-06-04 17:34:02 +0200

    Folders and CoordinateScript created
    Created some initial folders that will be needed troughout the project.
    Also started up a CoordinateScript that will be used to set the coordinates for the tiles.

---
+++ Assets/Scripts/CoordinateScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateScript : MonoBehaviour
+{
+    List<GameObject> yellowNest, blueNest, redNest, greenNest, whiteNest, blackNest;
+    int[,] position;
+    int numberOfTiles, offSet, xCrd, yCrd;
+    int[] newRow = { 1, 3, 6, 10, 23, 35, 46, 57, 65, 75, 86, 98, 111, 115, 118, 120, 121 };
+
+    void Start()
+    {
+        numberOfTiles = 121;
+        offSet = 1; 
+        for(int i = 0; i < numberOfTiles; i++)
+        {
+            for(int y = 0; y < newRow.Length; i++)
+            {
+                if(i == newRow[y])
+                {
+                    position[1, i] = y;
+                }
+            }
+            
+        }
+    }
+}
